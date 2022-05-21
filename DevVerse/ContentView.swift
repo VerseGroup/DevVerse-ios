13d828f0ca899250688c8b328c46146bb402192f
Steven Yu <stevenyuser@gmail.com>
2022-05-21 16:03:05 -0400

    Initial Commit

---
+++ DevVerse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DevVerse
+//
+//  Created by Steven Yu on 5/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DevVerse/DevVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevVerseApp.swift
+//  DevVerse
+//
+//  Created by Steven Yu on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct DevVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
